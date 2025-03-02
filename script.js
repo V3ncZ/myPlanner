86120e13aebcf528c1768f0008f129f67edfbc58
VenCz <viniciuscesarp3@gmail.com>
2025-03-02 20:15:43 -0300

    Ajustando o projetos

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ btnCriarNovoCardToDo.addEventListener("click", () => {
 
     let titulo = prompt("Qual o titulo da tarefa?");
     let descricao = prompt("Qual a descrição da tarefa?");
+    let prioridade = prompt("Qual a prioridade da tarefa?");
     const data = new Date().toLocaleDateString("pt-BR");
+
+    let prioridadeImg = "";
+    switch (prioridade.toLowerCase()) {
+        case "alta":
+            prioridadeImg = "./img/prioridade_alta.png";
+            break;
+        case "media":
+            prioridadeImg = "./img/prioridade_media.png";
+            break;
+        case "baixa":
+            prioridadeImg = "./img/prioridade_baixa.png";
+            break;
+    }
 
     let card = document.createElement("div");
     card.classList.add("toDo");
@@ -12,7 +26,10 @@ btnCriarNovoCardToDo.addEventListener("click", () => {
         <div class="holder">
             <h3 class="titulo_tarefa">${titulo}</h3>
             <p class="descricao">${descricao}</p>
-            <p class="data">${data}</p>
+            <div class="container_final">
+                <p class="data">${data}</p>
+                <img class="prioridade" src="${prioridadeImg}" alt="prioridade">
+            </div>
         </div>
     `;
     document.querySelector(".toDo").appendChild(card);
@@ -24,7 +41,21 @@ btnCriarNovoCardDoing.addEventListener("click", () => {
 
     let titulo = prompt("Qual o titulo da tarefa?");
     let descricao = prompt("Qual a descrição da tarefa?");
+    let prioridade = prompt("Qual a prioridade da tarefa?");
     const data = new Date().toLocaleDateString("pt-BR");
+
+    let prioridadeImg = "";
+    switch (prioridade.toLowerCase()) {
+        case "alta":
+            prioridadeImg = "./img/prioridade_alta.png";
+            break;
+        case "media":
+            prioridadeImg = "./img/prioridade_media.png";
+            break;
+        case "baixa":
+            prioridadeImg = "./img/prioridade_baixa.png";
+            break;
+    }
 
     let card = document.createElement("div");
     card.classList.add("doing");
@@ -32,7 +63,10 @@ btnCriarNovoCardDoing.addEventListener("click", () => {
         <div class="holder">
             <h3 class="titulo_tarefa">${titulo}</h3>
             <p class="descricao">${descricao}</p>
-            <p class="data">${data}</p>
+            <div class="container_final">
+                <p class="data">${data}</p>
+                <img class="prioridade" src="${prioridadeImg}" alt="prioridade">
+            </div>
         </div>
     `;
     document.querySelector(".doing").appendChild(card);
@@ -44,16 +78,39 @@ btnCriarNovoCardDone.addEventListener("click", () => {
 
     let titulo = prompt("Qual o titulo da tarefa?");
     let descricao = prompt("Qual a descrição da tarefa?");
+    let prioridade = prompt("Qual a prioridade da tarefa?");
     const data = new Date().toLocaleDateString("pt-BR");
 
-    let card = document.createElement("div");
+    let prioridadeImg = "";
+    if (prioridade) {
+    switch (prioridade.toLowerCase()) {
+        case "alta":
+            prioridadeImg = "./img/prioridade_alta.png";
+            break;
+        case "media":
+            prioridadeImg = "./img/prioridade_media.png";
+            break;
+        case "baixa":
+            prioridadeImg = "./img/prioridade_baixa.png";
+            break;
+    }
+    }
+    
+    if (titulo && descricao && prioridade) {
+        let card = document.createElement("div");
     card.classList.add("done");
     card.innerHTML = `
         <div class="holder">
             <h3 class="titulo_tarefa">${titulo}</h3>
             <p class="descricao">${descricao}</p>
-            <p class="data">${data}</p>
+            <div class="container_final">
+                <p class="data">${data}</p>
+                <img class="prioridade" src="${prioridadeImg}" alt="prioridade">
+            </div>
         </div>
     `;
     document.querySelector(".done").appendChild(card);
+    } else {
+        alert("Por favor, preencha todos os campos.");
+    }
 });
